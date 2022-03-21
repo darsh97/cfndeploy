564b31ed72244e0dbe89ffd71ecedc46cf5b1732
darsh97 <aravinddarshan13897@gmail.com>
2022-03-21 19:14:57 +0530

    4 products

--- add_to_s3.py
+++ add_to_s3.py
@@ -8,13 +8,11 @@ with open("lambda_src/products.json", "r") as config:
 
 def get_digest(file_path):
     h = hashlib.sha256()
-
     with open(file_path, 'rb') as file:
         while True:
             # Reading is buffered, so we can read smaller chunks.
             chunk = file.read(h.block_size)
-            if not chunk:
-                break
+            if not chunk: break
             h.update(chunk)
 
     return h.hexdigest()
