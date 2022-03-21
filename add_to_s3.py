957660d1d3dae62a237469753b61432b46ab9309
Darshan <aravinddarshan13897@outlook.com>
2022-03-21 12:08:41 +0530

    add code artifact to s3

--- add_to_s3.py
+++ add_to_s3.py
@@ -1,1 +1,20 @@
 import boto3
+from zipfile import ZipFile
+import json
+
+with open("products.json", "r") as config:
+    config_contents = json.load(config)
+
+s3_bucket_name = config_contents["bucket"]
+
+FILES = (
+    'lambda_function.py',
+    'products.json'
+)
+
+with ZipFile("code.zip", "w") as zip_file:
+    for f in FILES:
+        zip_file.write(f)
+
+s3 = boto3.resource('s3')
+s3.meta.client.upload_file("code.zip", s3_bucket_name, "code.zip")
--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,8 @@
 import json
-import products
 
 
 def lambda_handle(event, context):
-    with open('products.json', r) as f:
+    with open('products.json', 'r') as f:
         content = json.load(f)
         for product in content['products']:
             print(product)
