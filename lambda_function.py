e1ea2791e0d1d1180a5ecff1d71eec7e3fa8a045
darsh97 <aravinddarshan13897@gmail.com>
2022-03-21 14:26:23 +0530

    added cfn stack and lambda backed custom resource

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,58 @@
 import json
+import time
+import boto3
+import os
+import logging
+import cfnresponse
+from botocore.exceptions import ClientError
+
+s3_client = boto3.client('s3')
+
+with open("products.json", "r") as config_file:
+    config_content = json.load(config_file)
+    PRODUCT_NAMES = config_content.get("products")
+
+def lambda_handler(event, context):
+    try:
+        if event['RequestType'] == 'Create':
+            handle_create(event, context)
+        elif event['RequestType'] == 'Update':
+            handle_update(event, context)
+        elif event['RequestType'] == 'Delete':
+            handle_delete(event, context)
+    except ClientError as exception:
+        logging.error(exception)
+        cfnresponse.send(event, context, cfnresponse.FAILED,
+                         {}, error=str(exception))
 
 
-def lambda_handle(event, context):
-    with open('products.json', 'r') as f:
-        content = json.load(f)
-        for product in content['products']:
-            print(product)
+def handle_create(event, context):
+    create_folders()
+    cfnresponse.send(event, context, cfnresponse.SUCCESS,
+                     {})
+
+
+def handle_delete(event, context):
+    cfnresponse.send(event, context, cfnresponse.SUCCESS,
+                     {})
+
+
+def handle_update(event, context):
+    create_folders()
+    cfnresponse.send(event, context, cfnresponse.SUCCESS,
+                     {})
+
+
+def create_folders():
+    DUMMY_FILE = '/tmp/dummy.txt'
+
+    s3_bucket = os.environ['AWS_LAMBDA_FUNCTION_NAME']
+
+    with open(DUMMY_FILE, 'w'):
+        pass
+
+    for product_name in PRODUCT_NAMES:
+        print(f"creating folder {product_name}")
+        s3_client.upload_file(DUMMY_FILE, s3_bucket, f"{product_name}/")
+
+    return {"Status": "Completed"}
