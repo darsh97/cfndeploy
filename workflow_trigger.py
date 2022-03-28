a7fcad55faf395d315da2dc6a3201ffc8c99d03d
darsh97 <aravinddarshan13897@gmail.com>
2022-03-28 19:23:49 +0530

    setup trigger repo_dispatch

--- workflow_trigger.py
+++ workflow_trigger.py
@@ -4,6 +4,10 @@ import requests
 
 CONFIG_FILE: final = "./config.json"
 ACCESS_TOKEN: final = "access_token"
+OWNER: final = "owner"
+REPO: final = "repository"
+API: final = "api"
+TRIGGER_REQUEST: final = "trigger"
 
 
 def get_config_file_details(config_file_path: str):
@@ -15,8 +19,13 @@ def get_config_file_details(config_file_path: str):
 def lambda_handler(event, context):
     config = get_config_file_details(CONFIG_FILE)
     access_token = config[ACCESS_TOKEN]
+    owner = config[OWNER]
+    repo = config[REPO]
+    trigger_req_api = config[API][TRIGGER_REQUEST]
 
     headers = {
         "Authorization": f"token {access_token}",
         "Accept": "application/vnd.github.v3+json",
     }
+
+    trigger_requst: str = trigger_req_api.format(owner=owner, repo=repo)
